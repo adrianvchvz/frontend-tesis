17331154478761ea9c0d949d54f7397542bb61de
ADRIAN <al_19_alex_2001@hotmail.com>
2024-06-23 22:58:24 -0500

    Agregando boton reload

--- src/pages/Propuestas.jsx
+++ src/pages/Propuestas.jsx
@@ -1,8 +1,10 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useCallback } from "react";
 import Stepper from "../components/Stepper";
 import { Spinner } from "keep-react";
-import ParametersContext from '../ParametersContext';
+import ParametersContext from "../ParametersContext";
+import { Button } from "keep-react";
+import { IoReload } from "react-icons/io5";
 
 const SpinnerComponent = () => {
   return (
@@ -17,9 +19,10 @@ function Propuestas() {
   const [images, setImages] = useState([]);
   const [error, setError] = useState(null);
   const [selectedProposal, setSelectedProposal] = useState(null);
-  const [loadingImages, setLoadingImages] = useState([true, true]); 
+  const [loadingImages, setLoadingImages] = useState([true, true]);
 
-  useEffect(() => {
+  const fetchImages = useCallback(() => {
+    setLoadingImages([true, true]);
     fetch("https://3a60-34-171-207-244.ngrok-free.app/generate_plans", {
       method: "POST",
       headers: {
@@ -34,6 +37,7 @@ function Propuestas() {
           setLoadingImages([false, false]);
         } else {
           setError(data.message);
+          setLoadingImages([false, false]);
         }
       })
       .catch((error) => {
@@ -42,9 +46,13 @@ function Propuestas() {
       });
   }, [parameters]);
 
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
   const handleCheckboxChange = (index) => {
     setSelectedProposal(index);
-    localStorage.setItem('selectedProposalImage', images[index]); // Guardar la imagen seleccionada en localStorage
+    localStorage.setItem("selectedProposalImage", images[index]);
   };
 
   return (
@@ -53,38 +61,64 @@ function Propuestas() {
       {error ? (
         <div className="text-red-500 text-center">{error}</div>
       ) : (
-        <div className="grid grid-cols-2 gap-4 mt-6">
-          {loadingImages.map((loading, index) => (
-            <div key={index} className="flex flex-col items-center">
-              <div className="flex items-center h-72 w-72 justify-center">
-                {loading ? (
-                  <SpinnerComponent />
-                ) : (
-                  images[index] && (
-                    <img
-                      src={`data:image/png;base64,${images[index]}`}
-                      alt={`Propuesta ${index + 1}`}
-                      className="w-72 h-72 object-contain"
-                      onLoad={() => {
-                        const newLoadingImages = [...loadingImages];
-                        newLoadingImages[index] = false;
-                        setLoadingImages(newLoadingImages);
-                      }}
-                    />
-                  )
-                )}
-              </div>
-              <div className="mt-8 flex items-center">
-                <h2 className="text-sm font-semibold">PROPUESTA {index + 1}</h2>
-                <input
-                  type="checkbox"
-                  checked={selectedProposal === index}
-                  onChange={() => handleCheckboxChange(index)}
-                  className="ml-2"
-                />
-              </div>
+        <div className="flex justify-center items-center mt-6">
+          <div className="flex flex-col items-center">
+            <div className="flex items-center h-72 w-72 justify-center">
+              {loadingImages[0] ? (
+                <SpinnerComponent />
+              ) : (
+                images[0] && (
+                  <img
+                    src={`data:image/png;base64,${images[0]}`}
+                    alt="Propuesta 1"
+                    className="w-72 h-72 object-contain"
+                  />
+                )
+              )}
             </div>
-          ))}
+            <div className="mt-8 flex items-center">
+              <h2 className="text-sm font-semibold">PROPUESTA 1</h2>
+              <input
+                type="checkbox"
+                checked={selectedProposal === 0}
+                onChange={() => handleCheckboxChange(0)}
+                className="ml-2"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center mx-24">
+            <Button
+              size="sm"
+              onClick={fetchImages}
+              className="py-1 h-8 flex items-center justify-center mb-16"
+            >
+              <IoReload />
+            </Button>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="flex items-center h-72 w-72 justify-center">
+              {loadingImages[1] ? (
+                <SpinnerComponent />
+              ) : (
+                images[1] && (
+                  <img
+                    src={`data:image/png;base64,${images[1]}`}
+                    alt="Propuesta 2"
+                    className="w-72 h-72 object-contain"
+                  />
+                )
+              )}
+            </div>
+            <div className="mt-8 flex items-center">
+              <h2 className="text-sm font-semibold">PROPUESTA 2</h2>
+              <input
+                type="checkbox"
+                checked={selectedProposal === 1}
+                onChange={() => handleCheckboxChange(1)}
+                className="ml-2"
+              />
+            </div>
+          </div>
         </div>
       )}
     </div>
